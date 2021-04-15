7d8855637b0ea485fce707b231d94e507bf3c5a9
Shayla Sawchenko <ssawchenko@users.noreply.github.com>
2021-04-15 12:23:51 -0700

    Updated package name (#2)
    * Renamed app module package to versioncheckkotlinsample
    * Updated gitignore to remove all .idea files

--- app/src/androidTest/java/com/steamclock/versioncheckkotlinsample/ExampleInstrumentedTest.kt
+++ app/src/androidTest/java/com/steamclock/versioncheckkotlinsample/ExampleInstrumentedTest.kt
@@ -1,4 +1,4 @@
-package com.steamclock.versioncheckkotlin
+package com.steamclock.versioncheckkotlinsample
 
 import androidx.test.platform.app.InstrumentationRegistry
 import androidx.test.ext.junit.runners.AndroidJUnit4
